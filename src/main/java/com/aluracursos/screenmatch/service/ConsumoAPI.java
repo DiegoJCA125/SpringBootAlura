cd629422c8df9b1438ec339c1cddfd1030165d56
Juan Diego Carmona Arredondo <104167947+DiegoJCA125@users.noreply.github.com>
2024-05-28 15:38:52 -0500

    Add files via upload

--- src/main/java/com/aluracursos/screenmatch/service/ConsumoAPI.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsumoAPI.java
@@ -9,14 +9,16 @@ import java.net.http.HttpResponse;
 public class ConsumoAPI {
     public String obtenerDatos(String url){
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
         HttpResponse<String> response = null;
-
-        try{
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        }catch (IOException e){
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
             throw new RuntimeException(e);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         String json = response.body();
--- src/main/java/com/aluracursos/screenmatch/service/IConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/IConvierteDatos.java
@@ -1,6 +1,5 @@
 package com.aluracursos.screenmatch.service;
 
 public interface IConvierteDatos {
-    //convierte los datos genericos
     <T> T obtenerDatos(String json, Class<T> clase);
 }
